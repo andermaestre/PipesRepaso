11a31720c0ae763fa3640eae5fab3f83d9ea431e
Ander Maestre <andermaestre_90@hotmail.es>
2019-11-04 12:05:08 +0100

    Creando el repo

---
+++ PipesRepaso1/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Pipes;
+using System.IO;
+using System.Diagnostics;
+
+
+namespace PipesRepaso1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Process server = new Process();
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName="~\\Server.exe";
+            psi.UseShellExecute = false;
+            server.StartInfo=psi;
+            server.Start();
+
+            Process client = new Process();
+            ProcessStartInfo psi2 = new ProcessStartInfo();
+            psi2.FileName = "~\\Cliente.exe";
+            psi2.UseShellExecute = false;
+            client.StartInfo = psi2;
+            client.Start();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Pipes;
+using System.IO;
+using System.Diagnostics;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NamedPipeServerStream pipeform = new NamedPipeServerStream("pipeform", PipeDirection.Out);
+
+        }
+    }
+}
